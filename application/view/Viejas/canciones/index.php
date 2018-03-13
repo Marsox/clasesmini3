3a091d44575210477a1f6df5b791f2c4506cbfe2
Unknown <eduarmarso@hotmail.com>
2018-03-13 18:58:23 +0100

    Proyecto Vacio

--- application/view/Viejas/canciones/index.php
+++
@@ -1,9 +0,0 @@
-<div class="container">
-	<div class="container">
-		hola!!!
-	</div>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus numquam vitae minima fugiat iste, similique voluptas voluptatibus corporis vel iusto, mollitia repudiandae, fugit eius id perferendis illo! Minima, recusandae, inventore!</p>
-	<div class="container">
-		<?= APP ?>
-	</div>
-</div>
--- application/view/partials/banner.php
+++
@@ -1,5 +0,0 @@
-<div class="container">
-	Esto imita a un banner
-	<p><?= $dato ?></p>
-	<p><?= $titulo ?></p>
-</div>
