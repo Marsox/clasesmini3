8f040b6f1d5e0ecb0379b631f9fad2c961e00bb7
Unknown <eduarmarso@hotmail.com>
2018-03-13 19:18:49 +0100

    Modelo profesor

---
+++ application/Model/profesor.php
@@ -0,0 +1,49 @@
+<?php 
+
+class profesor {
+
+	private $email;
+	private $nombre;
+	private $apellidos;
+	private $md5password;
+	private $departamento;
+
+	private function __construct($email, $nombre, $apellidos, $md5password, $departamento){
+		$this->email = $email;
+		$this->nombre = $nombre;
+		$this->apellidos = $apellidos;
+		$this->md5password = $md5password;
+		$this->departamento = $departamento;
+	}
+	
+	public function getEmail() {
+		return $this->email;
+	}
+	public function getNombre(){
+		return $this->nombre;
+	}
+	public function getApellidos() {
+		return $this->apellidos;
+	}
+	public function getDepartamento() {
+		return $this->departamento;
+	}
+
+	public function setEmail($email){
+		$this->email = $email;
+	}
+	public function setNombre($nombre){
+		$this->nombre = $nombre;
+	}
+	public function setApellidos($apellidos){
+		$this->apellidos = $apellidos;
+	}
+	public function setDepartamento($departamento){
+		$this->departamento = $departamento;
+	}
+
+
+
+}
+
+?>
