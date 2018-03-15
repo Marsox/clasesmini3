e4b4a81b5c89c136b759fcb41366aafa924bbc18
Unknown <eduarmarso@hotmail.com>
2018-03-15 20:04:29 +0100

    Añadidas las urls para otros login/registro

--- application/view/profesor/loginform.php
+++ application/view/profesor/loginform.php
@@ -8,4 +8,6 @@
 		<input type="password" name="pass">
 	</p>
 <input type="submit" value="Login">
+<p>¿Eres Alumno? Accede <a href="<?=URL?>alumno/login">Aquí</a></p>
+<p>¿Eres Profesor y no tienes cuenta? Regístrate <a href="<?=URL?>profesor/register">Aquí</a></p>
 </form>
