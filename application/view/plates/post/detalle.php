2171849b9d7bc49a4208ba39012a90c2b50623d7
Unknown <eduarmarso@hotmail.com>
2018-04-12 19:23:18 +0200

    Detalle no revienta al visitarlo sin estar logueado

--- application/view/plates/post/detalle.php
+++ application/view/plates/post/detalle.php
@@ -9,7 +9,7 @@ use Mini\Model\Categoria;?>
 	<p>Cuerpo: <?= $post->getCuerpo() ?></p>
 	<p>Autor: <?= Profesor::getByEmail($post->emailAuthor)->getNombre().' '.Profesor::getByEmail($post->emailAuthor)->getApellidos() ?></p>
 	<p>Creado el: <?= date('d/m/Y', $post->getFecha())  ?></p>
-	<?php if($post->getEmailAuthor() == $_SESSION['user']->getEmail()){?>
+	<?php if(isset($_SESSION['user']) && $post->getEmailAuthor() == $_SESSION['user']->getEmail()){?>
 		<a href="<?php echo URL; ?>post/editar/<?= $post->getId() ?>">[ Editar ]</a>
 		<a href="<?php echo URL; ?>post/borrar/<?= $post->getId() ?>">[ Borrar ]</a>
 	<?php }?>
